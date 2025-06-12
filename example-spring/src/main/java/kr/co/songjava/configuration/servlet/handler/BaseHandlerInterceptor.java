f9a0d4be22b93896b256e1c0b4c792fd27a22c33
jins <djseo9812@gmail.com>
2025-06-12 22:30:50 +0900

    last

--- example-spring/src/main/java/kr/co/songjava/configuration/servlet/handler/BaseHandlerInterceptor.java
+++ example-spring/src/main/java/kr/co/songjava/configuration/servlet/handler/BaseHandlerInterceptor.java
@@ -1,11 +1,12 @@
 package kr.co.songjava.configuration.servlet.handler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class BaseHandlerInterceptor extends HandlerInterceptorAdapter {
 	
